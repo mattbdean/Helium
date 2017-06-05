f82f95780fed6e54147e99bc995495827990155f
Matthew Dean <deanmatthew16@gmail.com>
2017-06-05 18:48:18 -0400

    Enforce handling rejected promises in server tests

--- server/test/_root.ts
+++ server/test/_root.ts
@@ -1,5 +1,12 @@
 import { Database, Mode } from '../src/Database';
 
+// Catch unhandled Promises
+process.on('unhandledRejection', (reason) => {
+    process.stderr.write("Unhandled Promise rejection:\n");
+    console.error(reason);
+    process.exit(1);
+});
+
 // Mocha root suite. Install test hooks for all tests here.
 // https://mochajs.org/#root-level-hooks
 
