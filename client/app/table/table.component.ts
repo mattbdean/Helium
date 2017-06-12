e27069537711062e3b4870b6d33a27d4d0b0988b
Matthew Dean <deanmatthew16@gmail.com>
2017-06-12 16:05:17 -0400

    Format dates in the table

--- client/app/table/table.component.ts
+++ client/app/table/table.component.ts
@@ -3,6 +3,7 @@ import { Response } from '@angular/http';
 import { ActivatedRoute, Params } from '@angular/router';
 
 import * as _ from 'lodash';
+import * as moment from 'moment';
 
 import { SqlRow, TableHeader } from '../common/responses';
 import { TableService } from './table.service';
@@ -34,7 +35,7 @@ export class TableComponent implements OnInit {
             try {
                 const meta = await this.backend.meta(this.name);
                 this.headers = this.createTableHeaders(meta.headers);
-                this.content = await this.backend.content(this.name);
+                this.content = this.formatRows(meta.headers, await this.backend.content(this.name));
             } catch (e) {
                 // Handle 404s, show the user that the table couldn't be found
                 if (e instanceof Response && e.status === 404) {
@@ -50,5 +51,22 @@ export class TableComponent implements OnInit {
 
     private createTableHeaders(headers: TableHeader[]): DataTableHeader[] {
         return _.sortBy(_.map(headers, (h) => ({ name: h.name, prop: h.name })), 'ordinalPosition');
+    }
+
+    private formatRows(headers: TableHeader[], rows: SqlRow[]): SqlRow[] {
+        const copied = _.clone(rows);
+
+        // Iterate through each row
+        for (const row of copied) {
+            // Iterate through each cell in that row
+            for (const headerName of Object.keys(row)) {
+                const header = _.find(headers, (h) => h.name === headerName);
+                // Use moment to format dates
+                if (header.type === 'date')
+                    row[headerName] = moment(row[headerName]).format('l');
+            }
+        }
+
+        return copied;
     }
 }
