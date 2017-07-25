3d925d72f8113a14c8a7e86db60825b937e2a944
Matthew Dean <deanmatthew16@gmail.com>
2017-07-25 16:58:11 -0400

    Handle Accept: application/json for all routes

--- server/src/routes/front.ts
+++ server/src/routes/front.ts
@@ -1,17 +1,27 @@
 import { Request, Response, Router } from 'express';
 import * as path from 'path';
+import { ErrorResponse } from '../common/responses';
 
 export function front(): Router {
     const r = Router();
-
     r.get('*', (req: Request, res: Response) => {
-        res.sendFile(path.join(__dirname, '../public/index.html'), (err: Error) => {
-            if (err) {
-                return res.status(500)
-                    .contentType('text/plain')
-                    .send('This website didn\'t go through its build process properly');
-            }
-        });
+        if (req.accepts('html')) {
+            res.sendFile(path.join(__dirname, '../public/index.html'), (err: Error) => {
+                if (err) {
+                    return res.status(500)
+                        .contentType('text/plain')
+                        .send('This website didn\'t go through its build process properly');
+                }
+            });
+        } else if (req.accepts('json')) {
+            const resp: ErrorResponse = {
+                message: 'Not found',
+                input: {}
+            };
+            return res.status(404).json(resp);
+        } else {
+            return res.status(404).send('Please specify an Accept header');
+        }
     });
 
     return r;
