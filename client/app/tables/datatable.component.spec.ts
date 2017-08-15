e0694ff098a04f1a8e63473863b4d6d57f129925
Matthew Dean <deanmatthew16@gmail.com>
2017-08-15 17:35:10 -0400

    Add basic tests for DatatableComponent

---
+++ client/app/tables/datatable.component.spec.ts
@@ -0,0 +1,75 @@
+import { DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MdProgressBarModule } from '@angular/material';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+
+import { Observable } from 'rxjs/Observable';
+
+import * as sinon from 'sinon';
+
+import { By } from '@angular/platform-browser';
+import { SqlRow, TableMeta } from '../common/responses';
+import { CoreModule } from '../core/core.module';
+import { TableService } from '../core/table.service';
+import { DatatableComponent } from './datatable.component';
+
+const expect = global['chai'].expect;
+
+describe('DatatableComponent', () => {
+    let fixture: ComponentFixture<DatatableComponent>;
+    let comp: DatatableComponent;
+    let de: DebugElement;
+    let service: TableService;
+
+    const tableServiceStub = {
+        meta: (name: string): Observable<TableMeta> =>
+            Observable.throw(new Error('not stubbed')),
+        content: (name: string): Observable<SqlRow> =>
+            Observable.throw(new Error('not stubbed')),
+    };
+
+    const mockTableMeta = (): TableMeta => ({
+        headers: [],
+        totalRows: 0,
+        constraints: [],
+        comment: 'description'
+    });
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                CoreModule,
+                MdProgressBarModule,
+                NgxDatatableModule
+            ],
+            declarations: [
+                DatatableComponent
+            ],
+            providers: [
+                { provide: TableService, useValue: tableServiceStub }
+            ]
+        });
+
+        fixture = TestBed.createComponent(DatatableComponent);
+        comp = fixture.componentInstance;
+        de = fixture.debugElement;
+        service = de.injector.get(TableService);
+    });
+
+    it('should pull in the metadata when given a name', () => {
+        expect(comp.meta).to.be.null;
+        comp.name = 'foo';
+
+        const stub = sinon.stub(service, 'meta')
+            .returns(Observable.of<TableMeta>(mockTableMeta()));
+
+        fixture.detectChanges();
+        expect(stub).to.have.been.calledOnce;
+
+        // Header
+        expect(de.query(By.css('h1')).nativeElement.textContent).to.equal('foo');
+
+        // Description right under the header
+        expect(de.query(By.css('.table-description')).nativeElement.textContent).to.match(/description/);
+    });
+});
--- karma.conf.js
+++ karma.conf.js
@@ -4,20 +4,21 @@
 module.exports = function (config) {
     config.set({
         basePath: '',
-        frameworks: ['mocha', '@angular/cli'],
+        frameworks: ['mocha', 'sinon-chai', '@angular/cli'],
         plugins: [
             require('karma-mocha'),
-            require('karma-chai'),
+            require('karma-sinon-chai'),
+
             require('karma-mocha-clean-reporter'),
             require('karma-chrome-launcher'),
             require('karma-coverage-istanbul-reporter'),
+
             require('@angular/cli/plugins/karma')
         ],
         client: {
             clearContext: false
         },
         files: [
-            { pattern: 'node_modules/chai/chai.js', instrument: false },
             { pattern: 'node_modules/@angular/material/prebuilt-themes/deeppurple-amber.css', instrument: false },
         ],
         coverageIstanbulReporter: {
