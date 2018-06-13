df0935c147f054f97513f38b139a60f97830a1ab
Matthew Dean <deanmatthew16@gmail.com>
2018-06-13 11:10:31 -0400

    Fix #94

--- client/app/app-routing.module.ts
+++ client/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './not-found.component';
 
 const routes: Routes = [
-    // { path: '', redirectTo: '/tables', pathMatch: 'full' },
-    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '', redirectTo: '/tables', pathMatch: 'full' },
     { path: '**', component: NotFoundComponent }
 ];
 
