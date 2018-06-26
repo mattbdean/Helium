dbd8f2d1c433e306f17346214637f4f4a5116216
Matthew Dean <deanmatthew16@gmail.com>
2018-06-26 12:01:13 -0400

    Use hash routing in preview mode

--- client/app/app-routing.module.ts
+++ client/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { environment } from '../environments/environment';
 import { NotFoundComponent } from './not-found.component';
 
 const routes: Routes = [
@@ -10,7 +10,7 @@ const routes: Routes = [
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(routes)
+        RouterModule.forRoot(routes, { useHash: environment.preview })
     ],
     exports: [
         RouterModule
