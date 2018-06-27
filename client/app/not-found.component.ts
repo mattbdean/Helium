3fd3f2745de870416ec1463d8467ba77bca34ae9
Matthew Dean <deanmatthew16@gmail.com>
2018-06-27 08:02:57 -0400

    Fix #66

--- client/app/not-found.component.ts
+++ client/app/not-found.component.ts
@@ -1,6 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-    templateUrl: 'not-found.component.html'
+    template: `
+        <div class="container">
+            <h1 class="mat-display-1">Woah there!</h1>
+            <p>This page doesn't seem to exist</p>
+        </div>
+    `,
+    styles: [`
+        h1 {
+            margin: 0;
+        }
+
+        h1, p {
+            text-align: center;
+        }
+    `]
 })
 export class NotFoundComponent {}
