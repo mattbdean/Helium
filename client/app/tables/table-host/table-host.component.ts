d9fad62cd0412b0d432b288f9616176d6db6f790
Matthew Dean <deanmatthew16@gmail.com>
2018-08-30 13:14:24 -0400

    Fix #126

--- client/app/tables/table-host/table-host.component.ts
+++ client/app/tables/table-host/table-host.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { TableName } from '../../common/table-name';
+import { SidenavComponent } from '../../sidenav/sidenav.component';
 
 @Component({
     templateUrl: 'table-host.component.html',
@@ -9,13 +10,22 @@ import { TableName } from '../../common/table-name';
 export class TableHostComponent implements OnInit {
     public selectedTable: TableName | null;
 
-    public constructor(public route: ActivatedRoute) {}
+    public constructor(
+        private sidenav: SidenavComponent,
+        public route: ActivatedRoute
+    ) {}
 
     public ngOnInit(): void {
         this.route.params.subscribe((params: Params) => {
             // both /tables and /tables/:schema/:table route here, figure out if
             // we're provided data or not
             this.selectedTable = params.schema ? new TableName(params.schema, params.table) : null;
+
+            if (this.selectedTable === null) {
+                // Open the sidenav, manually ensure the sidenav is showable.
+                this.sidenav.showable = true;
+                this.sidenav.open();
+            }
         });
     }
 }
